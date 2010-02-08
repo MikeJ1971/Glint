a85eb15fc6c8e68078e8969b4f681f1c57ef7c5c
Mike Jones <mike.a.jones@bristol.ac.uk>
2010-02-08 14:06:01 +0000

    added license and author information

--- LinkedDataViewerAppDelegate.h
+++ LinkedDataViewerAppDelegate.h
@@ -28,6 +28,8 @@
   ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
   POSSIBILITY OF SUCH DAMAGE.
 */
+
+// Author: Mike Jones (mike.a.jones@bristol.ac.uk)
  
 #import <Cocoa/Cocoa.h>
 
